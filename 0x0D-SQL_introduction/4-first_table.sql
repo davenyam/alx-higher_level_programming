18bbc7c60e89ce91dc372ce1f82806f7f9585132
daverobeny <nyamwangedavid2019@gmail.com>
2024-08-04 07:37:48 +0300

    SQL Queries

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- creates a tabel first_table in current db
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- count and display number of records where id=89
+SELECT COUNT(id)
+FROM first_table
+WHERE id = 89;
